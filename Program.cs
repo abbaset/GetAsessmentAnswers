fa8662f88178b15e46a35c211c940a95280c26a3
abbaset <abbaset@hotmail.com>
2023-10-17 13:12:16 +0300

    Create AssessmentAnswer Model

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
+using GetAsessmentAnswers.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AssessmentsContext>(
+        options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
